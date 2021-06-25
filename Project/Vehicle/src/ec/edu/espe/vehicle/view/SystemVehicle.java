94cf02eefd23a882a251d399239465ff2332af97
GeraldiChasi <gmchasi@espe.edu.ec>
2021-06-24 21:59:03 -0500

    switch Chevrolet

--- Project/Vehicle/src/ec/edu/espe/vehicle/view/SystemVehicle.java
+++ Project/Vehicle/src/ec/edu/espe/vehicle/view/SystemVehicle.java
@@ -25,7 +25,7 @@ public class SystemVehicle {
       String color;
       Scanner read = new Scanner(System.in);
         int option;
-        
+        int option2; 
         
         System.out.println("Welcome to the dealership");
             System.out.println("Enter the money you have to purchase the vehicle:");
@@ -53,7 +53,51 @@ public class SystemVehicle {
                 System.out.println("2.-Sparkgt");
                 System.out.println("3.-Beat");
                 System.out.println("4.-Sail");
-                
+                option2=read.nextInt(); 
+             switch(option2){
+                 case 1:
+                 System.out.println("The Chevrolet Aveo version features:"); 
+                 System.out.println("1.5L 4-cylinder engine\n" +
+                  "    107 HP of power and 104 lb.-ft. of torque\n" +
+                  "    McPherson® front suspension\n" +
+                  "    5 speed manual transmission\n" +
+                  "    Air conditioning with manual control\n" +
+                  "    AM / FM radio, MP3 player, input for USB and Aux In and Bluetooth® connection\n" +
+                  "    14 ”steel wheels\n" +
+                  "    2 front airbags for driver and passenger\n" +
+                  "    4-wheel ABS brakes");
+                  System.out.println("Its price is: $15000");
+                    case 2:
+                 System.out.println("The Chevrolet Sparkgt version features:"); 
+                 System.out.println(" Gasoline engine\n" +
+                  "    Displacement 1999 cc\n" +
+                  "    Maximum Power 152 hp @ 6,200 rpm\n" +
+                  "    Maximum torque 191 nm @ 4,700 rpm\n" +
+                  "    6-speed manual transmission + reverse\n" +
+                  "    Driver and Passenger Airbag\n" +
+                  "    ABS anti-lock braking system");
+                  System.out.println("Its price is: $13000"); 
+                         case 3:
+                 System.out.println("The Chevrolet Beat version features:"); 
+                 System.out.println("1.2L 4-cylinder engine\n" +
+                  "   5-speed manual transmission\n" +
+                  "   Electro-assisted steering\n" +
+                  "   Mylink system with 7-inch touch screen (Compatible with Android Auto and Apple CarPlay)\n" +
+                  "   Air-conditioning\n" +
+                  "   Mirrors with electrical adjustment\n" +
+                  "   Electric windows on the 4 doors\n" +
+                  "   Audio and phone controls behind the wheel\n" +
+                  "   ABS + EBD braking systems\n" +
+                  "   Double front airbag");
+                  System.out.println("Its price is: $14.799"); 
+                              case 4:
+                 System.out.println("The Chevrolet Sail version features:"); 
+                 System.out.println("Slope start assistant\n" +
+                  "   Electronic stability control\n" +
+                  "   Aluminum rings\n" +
+                  "   Power: 109 hp");
+                  System.out.println("Its price is: $16000"); 
+             }  
             case 2:
                 System.out.println("Welcome to Toyota");
                 System.out.println("What model do you want of vehicle?");
@@ -81,8 +125,11 @@ public class SystemVehicle {
             default: System.out.println("WARNING!!!!");
             
         }
-            
+          
+                
+              
+            }
             }
             
-    }
+   
 
