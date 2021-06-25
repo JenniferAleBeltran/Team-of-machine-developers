68bff3b18a3b0b4d5294d04ed4aa63690a96e46f
benja1723 <84352819+benja1723@users.noreply.github.com>
2021-06-24 20:03:00 -0500

    Add Menu

--- Project/Vehicle/src/ec/edu/espe/vehicle/model/ModelofVehicle.java
+++ Project/Vehicle/src/ec/edu/espe/vehicle/model/ModelofVehicle.java
@@ -12,28 +12,33 @@ import java.util.ArrayList;
  * @author < Jennifer Beltran Team of machine Developers>
  */
 public class ModelofVehicle {
+    private String brand;
     private String name;
     private String color;
-    private String shape;
-    private ArrayList<Brand> brand = new ArrayList<>();
 
-//Construtor, toString(), Getter and Setter.
-    
-
-   public ModelofVehicle(String name, String color, String shape) {
+    public ModelofVehicle(String brand, String name, String color) {
+        this.brand = brand;
         this.name = name;
         this.color = color;
-        this.shape = shape;
-    }
-
-
-    public ModelofVehicle() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String toString() {
-        return "ModelOfVehicle{" + "name=" + getName() + ", color=" + getColor() + ", shape=" + getShape() + ", brand=" + getBrand() + '}';
+        return "ModelofVehicle{" + "brand=" + brand + ", name=" + name + ", color=" + color + '}';
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
     /**
@@ -63,35 +68,8 @@ public class ModelofVehicle {
     public void setColor(String color) {
         this.color = color;
     }
-
-    /**
-     * @return the shape
-     */
-    public String getShape() {
-        return shape;
-    }
-
-    /**
-     * @param shape the shape to set
-     */
-    public void setShape(String shape) {
-        this.shape = shape;
-    }
-
-    /**
-     * @return the brand
-     */
-    public ArrayList<Brand> getBrand() {
-        return brand;
-    }
-
-    /**
-     * @param brand the brand to set
-     */
-    public void setBrand(ArrayList<Brand> brand) {
-        this.brand = brand;
-    }
-
+    
+   
     
     
 }
--- Project/Vehicle/src/ec/edu/espe/vehicle/view/SystemVehicle.java
+++ Project/Vehicle/src/ec/edu/espe/vehicle/view/SystemVehicle.java
@@ -6,13 +6,10 @@
 package ec.edu.espe.vehicle.view;
 
 import ec.edu.espe.vehicle.model.Accessory;
-import ec.edu.espe.vehicle.model.Brand;
 import ec.edu.espe.vehicle.model.Client;
 import ec.edu.espe.vehicle.model.ModelofVehicle;
-import ec.edu.espe.vehicle.model.Type;
 import ec.edu.espe.vehicle.model.Vehicle;
 
-import ec.edu.espe.vehicle.model.*;
 import java.util.Scanner;
 
 
@@ -23,31 +20,11 @@ import java.util.Scanner;
 public class SystemVehicle {
         public static void main(String[] args  ){
         
-      Vehicle vehicle= new Vehicle("34000");
-      Client client= new Client("good", "Jennifer", "Quitumbe");
-
-      ModelofVehicle modelofvehicle= new ModelofVehicle("Toyota", "black", "squard");
-      Accessory accessory= new Accessory(5, 4, 1);
-      Brand brand= new Brand(25, 4, "Toyota");
-      Type type= new Type("Vehicle");
-      
-      vehicle.setPrice("2.000");
-      client.setEsconomicstatus("middle class ");
-      modelofvehicle.setName("Chevrolet");
-      accessory.setSeat(7);
-      brand.setAge(2004);
-      type.setT("t");
       float money;
-      String price;
+      String brand;
+      String color;
       Scanner read = new Scanner(System.in);
         int option;
-        
-      System.out.println("vehicle -> " + vehicle);
-      System.out.println("client -> " + client);
-      System.out.println("modelofvehicle -> " + modelofvehicle);
-      System.out.println("accessory -> " + accessory);
-      System.out.println("brand -> " + brand);
-      System.out.println("type -> " + type);
       
         System.out.println("Welcome to the dealership");
             System.out.println("Enter the money you have to purchase the vehicle:");
@@ -58,7 +35,18 @@ public class SystemVehicle {
             else{
             System.out.println("you can buy a new vehicle");  
             } 
-            System.out.println("You want a new car or a used car?");
+            System.out.println("What brand of vehicle do you want to buy?");
+            System.out.println("1.-Chevrolett");
+            System.out.println("2.-Toyota");
+            System.out.println("3.-KIA");
+            System.out.println("4.-Hyundai");
+            brand=read.next();
+            System.out.println("What color do you want your vehicle?");
+            System.out.println("1.-Red");
+            System.out.println("2.-White");
+            System.out.println("3.-Black");
+            System.out.println("4.-Gray");
+            color=read.next();
             
             }
             
