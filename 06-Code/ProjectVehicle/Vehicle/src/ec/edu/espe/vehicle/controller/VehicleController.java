23e083a6da6ca062f3ebd0ce107314103c6564d1
Vanessa Cayambe <vkcayambe@espe.edu.ec>
2021-08-03 21:53:34 -0500

    Interface

---
+++ 06-Code/ProjectVehicle/Vehicle/src/ec/edu/espe/vehicle/controller/VehicleController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.vehicle.controller;
+
+/**
+ *
+ * @author Vanessa Cayambe Team of machine ESPE-DCCO
+ */
+public class VehicleController implements VehicleDearleship {
+
+    @Override
+    public void price(float amount) {
+        System.out.println("The price of Vehicle is:");
+    }
+}
