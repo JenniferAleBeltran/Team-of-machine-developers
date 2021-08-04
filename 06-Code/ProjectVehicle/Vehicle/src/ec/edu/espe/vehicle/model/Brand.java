cbf90ce4c64189ab53cb001d48e039cbf745e5fc
benja1723 <84352819+benja1723@users.noreply.github.com>
2021-08-03 22:52:23 -0500

    create class Brand

---
+++ 06-Code/ProjectVehicle/Vehicle/src/ec/edu/espe/vehicle/model/Brand.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.vehicle.model;
+
+/**
+ *
+ * @author Benjamin Cadena OOPTech ESPE-DCCO
+ */
+public class Brand {
+    String nameOfBrand;
+
+    public Brand(String nameOfBrand) {
+        this.nameOfBrand = nameOfBrand;
+    }
+    
+    
+}
